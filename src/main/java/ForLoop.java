f235248271269cdae81aad319ab83d133e997582
Pavelec Ed (local) <local@test.ru>
2022-02-08 23:30:56 +0300

    test commit

--- src/main/java/ForLoop.java
+++ src/main/java/ForLoop.java
@@ -2,6 +2,8 @@
 
 public class ForLoop {
     public static void main(String[] args) {
-
+        for(int i = 0; i < 7; i = i + 1){
+            System.out.println("Это " + (i + 1) + " цикл for"); // i++ и i= i + 1 одно и тоже
+        }
     }
 }
