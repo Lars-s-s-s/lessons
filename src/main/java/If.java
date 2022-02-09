bed4fb810a366fbdb70b51ead2c01ed16d46fc84
Pavelec Ed (local) <local@test.ru>
2022-02-09 22:20:24 +0300

    test commit

--- src/main/java/If.java
+++ src/main/java/If.java
@@ -1,5 +1,10 @@
 public class If {
     public static void main(String[] args) {
-        //проверка работы github
+        int myInt = 11;
+        if(myInt<5){
+            System.out.println("Дменьше пяти");
+        } else if (myInt > 10){
+            System.out.println("больше десяти");
+        }
     }
 }
