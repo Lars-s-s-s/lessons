29413803eb84bb72b37951a7c30d518f3c6ad137
Pavelec Ed (local) <local@test.ru>
2022-02-10 23:28:58 +0300

    test commit

--- src/main/java/DoWhile.java
+++ src/main/java/DoWhile.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class DoWhile {
     public static void main(String[] args) {
+        /*
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите число с клавиатуры, для выхода - 777");
         int i = scan.nextInt();
@@ -9,6 +10,14 @@ public class DoWhile {
             System.out.println("Введите число с клавиатуры, для выхода - 777");
             i = scan.nextInt();
         }
+        System.out.println("Вышли из цикла");
+        */
+        Scanner scan = new Scanner(System.in);
+        int i;
+        do {
+            System.out.println("Введите число с клавиатуры, для выхода - 777");
+            i = scan.nextInt();
+        } while(i != 777);
         System.out.println("Вышли из цикла");
     }
 }
