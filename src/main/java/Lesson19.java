b8e43c789cdaff2141ae46321b9f925a3a06e78c
Pavelec Ed (local) <local@test.ru>
2022-02-21 10:14:26 +0300

    test commit

---
+++ src/main/java/Lesson19.java
@@ -0,0 +1,15 @@
+// Lesson 19 - Ключевое слово this
+
+public class Lesson19 {
+}
+class Human{
+    String name;
+    int age;
+
+    public void setName(String myName) {
+        name = myName;
+    }
+    public void setAge(int myAge) {
+        age = myAge;
+    }
+}
