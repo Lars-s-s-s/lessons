da41bb869b562931d642626be154e75a2ab26b5f
Pavelec Ed (local) <local@test.ru>
2022-02-23 14:22:46 +0300

    test commit

--- src/main/java/Lesson21.java
+++ src/main/java/Lesson21.java
@@ -2,23 +2,15 @@
 
 public class Lesson21 {
     public static void main(String[] args) {
+    Human21 chel = new Human21();
 
     }
 }
 class Human21{
 
-    String name; // переменные объекта. пока нет созданного объекта нет и переменной
-    int age;
+    private String name; // переменные объекта. пока нет созданного объекта нет и переменной
+    private int age;
 
-    public Human21() {
-        System.out.println("Это вывод из конструктора по умолчанию");
-        this.name = "name default";
-        this.age = 777;
-    }
-    public Human21(String name){
-        this.name = name;
-        System.out.println("Это вывод из второго конструктора");
-    }
     public Human21(String name, int age){
         this.name = name;
         this.age = age;
