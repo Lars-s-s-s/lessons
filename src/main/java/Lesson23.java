c0825f7bf99097d9e41d0cfbe43841253f566cb1
Pavelec Ed (local) <local@test.ru>
2022-05-04 23:03:51 +0300

    Lesson 22 - Ключевое слово final

---
+++ src/main/java/Lesson23.java
@@ -0,0 +1,13 @@
+// Lesson 23 - StringBuilder
+
+public class Lesson23 {
+    public static void main(String[] args) {
+        System.out.println("Привет"); // Переводит строку
+        System.out.print("Привет, ещё раз!");  // Не переводит строку
+        System.out.println(" Пока");
+
+        System.out.printf("Это строка, %s", "Привет" ); //форматирует строку// %s - подставляет на это место переменную, %d - цифра
+        System.out.println();
+        System.out.printf("Это строка, %d", 10 );
+    }
+}
