f07fbe39bf3e20d9b6e81feaa34497e3ef87c87f
Ed P Idea new <Lars1975@mail.com>
2024-02-20 14:49:36 +0300

    Мелкая правка описки

--- src/main/java/If.java
+++ src/main/java/If.java
@@ -2,7 +2,7 @@ public class If {
     public static void main(String[] args) {
         int myInt = 11;
         if(myInt<5){
-            System.out.println("Дменьше пяти");
+            System.out.println("Меньше пяти");
         } else if (myInt > 10){
             System.out.println("больше десяти");
         }
